e6725fab7ea763500698da85b61ace38b3eece1c
noguhiro <g007c1280@gmail.com>
2015-11-16 23:30:41 +0900

    update index.php, add custom.css

--- index.php
+++ index.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>NearFoods | 近くの食事処を簡単に探せる！</title>
+  <meta charset="utf-8">
+  <title>NearFoods | 近くの食事処を簡単に探せる！</title>
 
-    <!-- Include the compiled Ratchet CSS -->
-    <link href="./assets/css/ratchet.css" rel="stylesheet">
+  <!-- Include the compiled Ratchet CSS -->
+  <link href="./assets/css/ratchet.css" rel="stylesheet">
+  <link href="./assets/css/custom.css" rel="stylesheet">
 
-    <!-- Include the compiled Ratchet JS -->
-    <script src="./assets/js/ratchet.js"></script>
+  <!-- Include the compiled Ratchet JS -->
+  <script src="./assets/js/ratchet.js"></script>
 </head>
 <body>
-<div align="left" class="content-padded">
-    <span class="icon icon-bars"></span>NearFoods
+<div class="nf-nav">
+  <span class="icon icon-bars"></span>
+  <span class="title-text">NearFoods</span>
 </div>
 <div align="center">
-    <h2>近場の食事処を</h2>
+  <h2>近場の食事処を</h2>
 </div>
 <form class="input-group">
-<button class="btn btn-negative btn-block">探す</button>
-</br>
-        <h3>徒歩</h3><input type="text" placeholder="５分以内の食事処を表示">
-        <h3>徒歩</h3><input type="text" placeholder="５分以内の食事処を表示">
-        <h3>徒歩</h3><input type="text" placeholder="５分以内の食事処を表示">
+  <button class="btn btn-negative btn-block">探す</button>
+  <h3>徒歩</h3><input type="text" placeholder="５分以内の食事処を表示">
+  <h3>徒歩</h3><input type="text" placeholder="５分以内の食事処を表示">
+  <h3>徒歩</h3><input type="text" placeholder="５分以内の食事処を表示">
 </form>
 </body>
 </html>
