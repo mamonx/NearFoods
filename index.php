3bc30aea8901dfdd0c414a9dd288f729456fa7d2
mamonx <metalmacho11@hotmail.com>
2015-11-16 17:25:37 +0900

    1

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Ratchet template page</title>
+
+    <!-- Sets initial viewport load and disables zooming  -->
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+
+    <!-- Makes your prototype chrome-less once bookmarked to your phone's home screen -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+    <!-- Include the compiled Ratchet CSS -->
+    <link href="./assets/css/ratchet.css" rel="stylesheet">
+
+    <!-- Include the compiled Ratchet JS -->
+    <script src="./assets/js/ratchet.js"></script>
+</head>
+<body>
+<div align="left" class="content-padded">
+    <span class="icon icon-bars"></span>
+</div>
+<div align="center">
+    <h2>近場の食事処を</h2>
+</div>
+</br>
+<button class="btn btn-negative btn-block">探す</button>
+</body>
+</html>
