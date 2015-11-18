865162df891e5137603b0a6571c9a06fe78c73d3
mamonx <metalmacho11@hotmail.com>
2015-11-18 21:47:59 +0900

    1213

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>検索結果</title>
+  <link href="{{ url('assets/css/ratchet.css') }}" rel="stylesheet">
   <link href="{{ url('assets/css/custom.css') }}" rel="stylesheet">
   <script src="{{ url('assets/js/ratchet.js') }}"></script>
 </head>
@@ -14,27 +15,34 @@
 @if (isset($shops['Shop']))
   @foreach($shops['Shop'] as $shop)
     <div class="demo">
-      <h4>{{ $shop['ShopName'] }}</h4>
-      <a href="{{ url('/shops/' . $shop['ShopIdFront']) }}">
-        <img src="{{ $shop['PictureUrl']['PcMiddleImg'] }}" alt="a"/>
-      </a>
-      <table>
-        <tr>
-          <th>住所</th>
-          <td>{{ $shop['ShopAddress'] }}</td>
-        </tr>
-        <tr>
-          <th>予算</th>
-          <td>{{ $shop['BudgetDesc'] }}</td>
-        </tr>
-        <tr>
-          <th>駐車場</th>
-          <td>{{ $shop['Parking'] }}</td>
-        </tr>
-      </table>
-    </div>
-    <hr/>
+        <ul class="table-view">
+            <li class="table-view-cell media">
+                <h4>{{ $shop['ShopName'] }}</h4>
+                <a class="navigate-right">
+                    <a href="{{ url('/shops/' . $shop['ShopIdFront']) }}">
+                    <img class="media-object pull-left" src="{{ $shop['PictureUrl']['PcMiddleImg'] }}" alt="a"/>
+                    </a>
+                    <div class="media-body">
+                        <table>
+                            <tr>
+                                <th>住所</th>
+                                <td>{{ $shop['ShopAddress'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>予算</th>
+                                <td>{{ $shop['BudgetDesc'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>駐車場</th>
+                                <td>{{ $shop['Parking'] }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </a>
+            </li>
   @endforeach
+        </ul>
+        </div>
 @else
   最寄りの店舗が見つかりませんでした。
 @endif
