9d6f32fc77b036e451994eaf66c3defefa1fa2f3
noguhiro <g007c1280@gmail.com>
2015-11-18 18:44:32 +0900

    bug fixed index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,30 +15,26 @@
 <div align="center">
   <h2>近場の食事処を</h2>
 </div>
-<<<<<<< HEAD
-<form class="input-group">
-    <button class="btn btn-negative btn-block">探す</button>
-=======
 <form action="{{ url('shops') }}" class="input-group" method="post">
->>>>>>> b242b5b3203a1956d8206bfa0c7db43ce4639549
-    <select>
-        <option value="on">営業中の食事処のみ表示</option>
-        <option value="off">すべての食事処を表示</option>
-    </select>
-    <select>
-        <option value="5">徒歩５分以内</option>
-        <option value="10">徒歩１０分以内</option>
-    </select>
-    <div class="restaurant" align="right">
-        食事処の営業状況：
-        <span class="open">OPEN</span>
-        <span class="close">CLOSE</span>
-    </div>
-    <div class="frame">
-        <input type="text" value="食事処を表示">
-        <input type="text" value="営業時間">
-        <input type="text" value="所要時間">
-    </div>
+  <button class="btn btn-negative btn-block">探す</button>
+  <select>
+    <option value="on">営業中の食事処のみ表示</option>
+    <option value="off">すべての食事処を表示</option>
+  </select>
+  <select>
+    <option value="5">徒歩５分以内</option>
+    <option value="10">徒歩１０分以内</option>
+  </select>
+  <div class="restaurant" align="right">
+    食事処の営業状況：
+    <span class="open">OPEN</span>
+    <span class="close">CLOSE</span>
+  </div>
+  <div class="frame">
+    <input type="text" value="食事処を表示">
+    <input type="text" value="営業時間">
+    <input type="text" value="所要時間">
+  </div>
   <input name="lat" value="35.529274" type="hidden"/>
   <input name="lng" value="139.375055" type="hidden"/>
 </form>
