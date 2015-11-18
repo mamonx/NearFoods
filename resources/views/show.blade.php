8eb5ab3cb46b273a0816c3921ee28beed27b5a2b
mamonx <metalmacho11@hotmail.com>
2015-11-19 02:48:06 +0900

    12313

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -30,6 +30,98 @@
               <th>住所</th>
               <td>{{ $shop["ShopAddress"] }}</td>
           </tr>
+          <tr>
+              <th>最寄り駅</th>
+              <td>{{ $shop["StationName"] }}</td>
+          </tr>
+          <tr>
+              <th>都道府県</th>
+              <td>{{ $shop["ServiceAreaName"] }}</td>
+          </tr>
+          <tr>
+              <th>ジャンル</th>
+              <td>{{ $shop["GenreName"] }}</td>
+          </tr>
+          <tr>
+              <th>料理</th>
+              <td>{{ $shop["FoodName"] }}</td>
+          </tr>
+          <tr>
+              <th>予算</th>
+              <td>{{ $shop["BudgetDesc"] }}</td>
+          </tr>
+          <tr>
+              <th>平均予算</th>
+              <td>{{ $shop["BudgetAverage"] }}</td>
+          </tr>
+          <tr>
+              <th>アクセス</th>
+              <td>{{ $shop["Access"] }}</td>
+          </tr>
+          <tr>
+              <th>営業日&nbsp;営業時間</th>
+              <td>{{ $shop["Open"] }}</td>
+          </tr>
+          <tr>
+              <th>休み</th>
+              <td>{{ $shop["Close"] }}</td>
+          </tr>
+          <tr>
+              <th>コース料理</th>
+              <td>{{ $shop["Course"] }}</td>
+          </tr>
+          <tr>
+              <th>飲み放題</th>
+              <td>{{ $shop["FreeDrink"] }}</td>
+          </tr>
+          <tr>
+              <th>食べ放題</th>
+              <td>{{ $shop["FreeFood"] }}</td>
+          </tr>
+          <tr>
+              <th>個室</th>
+              <td>{{ $shop["PrivateRoom"] }}</td>
+          </tr>
+          <tr>
+              <th>掘りごたつ</th>
+              <td>{{ $shop["Horigotatsu"] }}</td>
+          </tr>
+          <tr>
+              <th>畳</th>
+              <td>{{ $shop["Tatami"] }}</td>
+          </tr>
+          <tr>
+              <th>カード支払い</th>
+              <td>{{ $shop["Card"] }}</td>
+          </tr>
+          <tr>
+              <th>喫煙</th>
+              <td>{{ $shop["NonSmoking"] }}</td>
+          </tr>
+          <tr>
+              <th>貸切</th>
+              <td>{{ $shop["Charter"] }}</td>
+          </tr>
+          <tr>
+              <th>駐車場</th>
+              <td>{{ $shop["Parking"] }}</td>
+          </tr>
+          <tr>
+              <th>バリアフリー</th>
+              <td>{{ $shop["BarrierFree"] }}</td>
+          </tr>
+          <tr>
+              <th>カラオケ</th>
+              <td>{{ $shop["Karaoke"] }}</td>
+          </tr>
+          <tr>
+              <th>ペット</th>
+              <td>{{ $shop["Pet"] }}</td>
+          </tr>
+          <tr>
+              <th>お子様</th>
+              <td>{{ $shop["Child"] }}</td>
+          </tr>
       </table>
   </div>
 @else
